7465a3d984d1eb39876a3a129508e29ac7f29707
alienwizard <johanwendin@gmail.com>
2016-10-07 08:52:35 +0200

    push db

--- wp-content/themes/design5_simple/page-templates/contact.php
+++ wp-content/themes/design5_simple/page-templates/contact.php
@@ -396,7 +396,7 @@ $(document).ready(function(){
 	    		<div class="col-sm-7">
 	    			<h2 class="sub_header"><?php the_title();?></h2>
 	    			<div>
-	    				<?php echo do_shortcode(''. the_field('kontaktformular') .''); ?>
+	    				<?php echo do_shortcode(the_field('kontaktformular')); ?>
 	    			</div>
 	    		</div>
 	    		<div class="col-sm-5">
